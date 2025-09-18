89c1c057ff8df53bbf0e7e7a1dfe1e8b74daa6b0
Goncalo Palma Nunes <goncalopalmanunes@tecnico.ulisboa.pt>
2025-09-18 15:48:39 +0100

    feat: create InferenceAlgorithm interface

---
+++ src/main/java/optimalarborescence/inference/InferenceAlgorithm.java
@@ -0,0 +1,17 @@
+package optimalarborescence.inference;
+
+import optimalarborescence.graph.Graph;
+
+public interface InferenceAlgorithm {
+
+
+    /**
+     * Infers a phylogenetic tree from the given graph.
+     *
+     * @param graph the input graph
+     * @return the inferred phylogenetic tree as a graph
+     */
+    Graph inferPhylogeny(Graph graph);
+
+    
+}
